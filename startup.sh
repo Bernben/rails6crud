da6f2676f20a34351a522eafc1543cde79dcbf7c
bernben <beni1meier@gmail.com>
2021-02-17 15:26:23 +0100

    changes

--- startup.sh
+++ startup.sh
@@ -4,5 +4,4 @@ if [ ! -d "/root/.ssh/" ]; then
     ln -s /var/lib/secrets/is_rsa /root/.ssh/id_rsa
 fi
 PWD=$(pwd)
-# rm ${PWD}/tmp/pids/server.pid comm
 LANG=DE rails s -b 0.0.0.0 >>/${PWD}/log/rails_out 2>> ${PWD}/log/rails_err
